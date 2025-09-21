117379d48e61a96fef908f0e496ba94541e55af8
Adam Vlček <vlcek-adam@seznam.cz>
2025-09-21 18:36:37 +0200

    added hand shake url

--- main.go
+++ main.go
@@ -29,6 +29,14 @@ func main() {
 		w.Write([]byte(fmt.Sprintf("%f", percentage*100)))
 	})
 
+	http.HandleFunc("/handshake", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hand shaken"))
+	})
+
 	list, _ := net.Listen("tcp", ":8040")
 	fmt.Println("Listening on port 8040")
 	http.Serve(list, nil)
