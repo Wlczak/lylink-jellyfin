807bac2ce36016312c311ca0d02d47071b53eb69
Adam Vlček <vlcek-adam@seznam.cz>
2025-09-21 18:46:03 +0200

    switched over to gin

--- main.go
+++ main.go
@@ -2,42 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 
-	"github.com/Wlczak/lylink-jellyfin/api"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		api, err := api.NewApi("username", "password")
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		item, _ := api.GetPlaybackInfo()
-
-		w.Header().Set("Content-Type", "text/plain")
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		w.WriteHeader(http.StatusOK)
-
-		percentage := float64(item.PlayState.PositionTicks) / float64(item.NowPlayingItem.RunTimeTicks+1)
-		w.Write([]byte(fmt.Sprintf("%f", percentage*100)))
+	r.GET("/handshake", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.String(http.StatusOK, "hand shaken")
 	})
 
-	http.HandleFunc("/handshake", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("hand shaken"))
-	})
-
-	list, _ := net.Listen("tcp", ":8040")
-	fmt.Println("Listening on port 8040")
-	http.Serve(list, nil)
+	fmt.Println("Listening on port :8040")
+	r.Run(":8040")
 }
