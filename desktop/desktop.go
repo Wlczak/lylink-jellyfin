9781441d940ad77d95c283e13c282a05f6661928
Adam Vlček <vlcek-adam@seznam.cz>
2025-10-02 10:59:16 +0200

    errcheck

--- desktop/desktop.go
+++ desktop/desktop.go
@@ -121,7 +121,11 @@ func setupConfigWindow() {
 		}
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
-		server.Shutdown(ctx)
+		err = server.Shutdown(ctx)
+
+		if err != nil {
+			zap.Error(err.Error())
+		}
 
 		cancel()
 
